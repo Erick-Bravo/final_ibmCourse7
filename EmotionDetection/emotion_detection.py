fc547c65fa7307684ab9312e24ff6ee190924965
Erick-Bravo <erickbrvo@gmail.com>
2024-03-03 01:37:40 -0800

    pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,8 +9,13 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=20)
     formatted_response = json.loads(response.text)
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
-    dominant_emotion = max(emotions, key=lambda k: emotions[k])
-    emotions['dominant_emotion'] = dominant_emotion
+    output = {}
+    if (response.status_code == 400):
+        output = {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+    if (response.status_code == 200):
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+        dominant_emotion = max(emotions, key=lambda k: emotions[k])
+        emotions['dominant_emotion'] = dominant_emotion
+        output = emotions
 
-    return emotions
+    return output
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+This file is routes in our emotion detection api, using BARD emotion detection
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -13,8 +16,14 @@ def emo_detector():
     joy = response["joy"]
     sadness = response["sadness"]
     dominant_emotion = response["dominant_emotion"]
-    
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}"
+
+    if dominant_emotion is None:
+        return "Invalid input! Try again."
+    text_1 = "For the given statement, the system response is "
+    data_1 = f"'anger': {anger}, 'disgust': {disgust}, "
+    data_2 = f"'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+    text_2 = f"The dominant emotion is {dominant_emotion}"
+    return text_1 + data_1 + data_2 + text_2
 
 @app.route("/")
 def render_index_page():
